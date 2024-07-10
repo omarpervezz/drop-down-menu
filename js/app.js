b195a9d65f03801a0e69b982ac5b78c034957d1c
Omar Pervez <freelancertusar5182@gmail.com>
2024-07-10 12:51:32 +0600

    added the work

---
+++ js/app.js
@@ -0,0 +1,45 @@
+(function ($) {
+  // Begin jQuery
+  $(function () {
+    // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").toggle();
+      // Close one dropdown when selecting another
+      $(".nav-dropdown").not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
+    });
+    // Toggle open and close nav styles on click
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
+    // Hamburger to X toggle
+    $("#nav-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuItems = document.querySelectorAll(".menu-item");
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      // If the clicked item already has the icon, remove it and return
+      if (this.classList.contains("icon-added")) {
+        this.classList.remove("icon-added");
+        return;
+      }
+
+      // Remove the icon-added class from all menu items
+      menuItems.forEach((i) => i.classList.remove("icon-added"));
+
+      // Add the icon to the clicked menu item
+      this.classList.add("icon-added");
+    });
+  });
+});
